ba8ae82074adee538ec5da9e6b0984e6a690537c
Aster <asterrdh@live.cn>
2019-09-28 13:54:41 +0800

    民宿详情

--- app/src/main/java/com/bcu/ccshop/activity/HomeInfo.java
+++ app/src/main/java/com/bcu/ccshop/activity/HomeInfo.java
@@ -1,34 +1,29 @@
 package com.bcu.ccshop.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import androidx.core.view.ViewCompat;
-import androidx.core.widget.NestedScrollView;
-import androidx.databinding.library.BuildConfig;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.TextView;
 
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
 import com.bcu.ccshop.R;
 import com.bcu.ccshop.dataTranformer.Homestay;
-import com.bcu.ccshop.dataTranformer.goods;
 import com.bigkoo.convenientbanner.ConvenientBanner;
 import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
+
 import com.google.android.material.appbar.AppBarLayout;
-import com.google.android.material.appbar.CollapsingToolbarLayout;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import cn.hutool.http.HttpException;
@@ -46,6 +41,10 @@ public class HomeInfo extends AppCompatActivity {
     private ConvenientBanner convenientBanner;
     private TextView hName,hSpec,hSize,hCapacity,hBed,hInfo,hCheckInDate,hCheckOutDate,hDays,hPrice,hADD;
     private String[] xy={"0","0"};
+    private int COMPLETED3 = 3;
+    private String hPhone;
+    private MapView mMapView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,11 +57,17 @@ public class HomeInfo extends AppCompatActivity {
         hCapacity= findViewById(R.id.textView39);
         hBed=findViewById(R.id.textView40) ;
         hInfo= findViewById(R.id.textView42);
-        hCheckInDate = findViewById(R.id.guideline27);
+        hCheckInDate = findViewById(R.id.textView29);
         hCheckOutDate= findViewById(R.id.textView30);
         hDays= findViewById(R.id.textView31);
         hPrice=findViewById(R.id.textView43);
         hADD=findViewById(R.id.textView45);
+        mMapView=findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+        AMap aMap = mMapView.getMap();
+        aMap.setTrafficEnabled(true);// 显示实时交通状况
+        //地图模式可选类型：MAP_TYPE_NORMAL,MAP_TYPE_SATELLITE,MAP_TYPE_NIGHT
+        aMap.setMapType(AMap.MAP_TYPE_SATELLITE);// 卫星地图模式
 
 
     }
@@ -83,23 +88,15 @@ public class HomeInfo extends AppCompatActivity {
                     System.out.println(result);
                     Gson gson=new Gson();
                     homestay=gson.fromJson(result, new TypeToken<Homestay>(){}.getType());
-                    hName.setText(homestay.gethName());
-                    hSpec.setText(homestay.gethRoom()+"室 "+homestay.gethHall()+"厅 "+homestay.gethToilet()+"卫");
-                    hSize.setText(homestay.gethSize()+"㎡");
-                    hCapacity.setText("可容纳："+homestay.gethPeople()+"人");
-                    hBed.setText(homestay.gethBedCount()+"张床");
-                    hInfo.setText(homestay.gethDescription());
-                    hPrice.setText(homestay.gethPrice()+"元/晚");
-                    hADD.setText(homestay.gethAddress());
                     images = jsonObject.getStr("hDetailImage").split(";");
                     xy=jsonObject.getStr("hPosition").split(";");
                     for (String s : images) {
                         s=MainActivity.SERVER_URL+"/"+s;
                         imgList.add(s);
                     }
-                    Message m = new Message();
-                    handler.handleMessage(m);
-
+                    Message message3 = new Message();
+                    message3.what=COMPLETED3;
+                    handler.sendMessage(message3);
                 } catch (HttpException e) {
                     e.printStackTrace();
                 }
@@ -107,15 +104,55 @@ public class HomeInfo extends AppCompatActivity {
         }
         ).start();
 
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
     }
 
-    Handler handler = new Handler() {
+
+
+    private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             //   System.out.println(JSONUtil.toJsonStr(imgList));
-            doRefresh();
-        };
-
+            if(msg.what==COMPLETED3){
+                hName.setText(homestay.gethName());
+                String LinShi;
+                LinShi=homestay.gethRoom()+"室 "+homestay.gethHall()+"厅 "+homestay.gethToilet()+"卫";
+                hSpec.setText(LinShi);
+                hSize.setText(homestay.gethSize()+"㎡");
+                hCapacity.setText("可容纳："+homestay.gethPeople()+"人");
+                hBed.setText(homestay.gethBedCount()+"张床");
+                hInfo.setText(homestay.gethDescription());
+                hPrice.setText(homestay.gethPrice()+"元/晚");
+                hADD.setText(homestay.gethAddress());
+                hPhone=homestay.gethPhone();
+                System.out.println("phone"+hPhone);
+                doRefresh();
+            }
+        }
     };
 
     void doRefresh() {
@@ -128,7 +165,7 @@ public class HomeInfo extends AppCompatActivity {
                         return new NetImageHolderView();
                     }
                 }, imgList).setPointViewVisible(true)
-                        .setPageIndicator(new int[]{R.drawable.icon_no_select, R.drawable.icon_on_select})
+                        //.setPageIndicator(new int[]{R.drawable.icon_no_select, R.drawable.icon_on_select})
                         .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.ALIGN_PARENT_RIGHT);
             }
         });
@@ -139,6 +176,13 @@ public class HomeInfo extends AppCompatActivity {
         Intent intent=new Intent(Intent.ACTION_VIEW,uri);
         startActivity(intent);
     }
+    public void goPhone(View view){
+        Uri uri= Uri.parse("tel:"+homestay.gethPhone());
+        Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+    }
+
+
 
 
 }
